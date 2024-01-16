8aff1800cf80906eb1646446026f9db2d4983b25
Felix Lee <felix.yy.lee@gmail.com>
2024-01-15 23:41:35 -0500

    Finish AddTask.js
    Finish step a-g for AddTask.js

---
+++ ToDoList/components/AddTask.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+const AddTask = ({ onAddTask }) => {
+    const [title, settitle] = useState('');
+
+    const handleAddTask = () => {
+        if (title.trim() !== '') {
+            onAddTask(Text(title));
+            settitle('');
+        }
+    };
+
+    return (
+        <View style={addTodoForm}>
+            <TextInput
+             style={styles.input}
+             placeholder="Enter to-do task"
+             value={title}
+             onChangeText={(text) => settitle(text)}
+             keyboardType="default"
+             returnKeyType="done" 
+            />
+            <Button title="Add to-do task" onPress={handleAddTask} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    addTodoForm: {
+      margin: 10,
+    },
+    input: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 10,
+      paddingHorizontal: 10,
+    },
+});
+
+export default AddTask;
